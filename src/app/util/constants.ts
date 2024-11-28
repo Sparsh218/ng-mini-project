b889a62272f1c16af7054deaaddd1fa95328535b
Sparsh Jain <sparsh.jain@cloudsmartz.net>
2024-11-28 14:45:00 +0530

    Hide secret key

--- src/app/util/constants.ts
+++ src/app/util/constants.ts
@@ -1,6 +1,6 @@
 export class Constants {
 
-    private static readonly apiKey: string = "AIzaSyApl5CC1OZ9WqDE59-giSdyDctURkHnqrM";
+    private static readonly apiKey: string = "";
 
     static readonly signUpUrl: string = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key="+this.apiKey;
 
